789b0d0ba51c2acf37a8ac08d81c725f9cb95121
Zinedine038 <zsghari@hotmail.com>
2020-02-18 00:23:46 +0100

    Test Commit

--- src/MySweetProgram.java
+++ src/MySweetProgram.java
@@ -2,6 +2,6 @@ public class MySweetProgram
 {
     public static void main(String[] args)
     {
-        System.out.println("Huts a Niffo");
+        System.out.println("G E K O L O N I S E E R D");
     }
 }
